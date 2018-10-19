25ead4e77401613b4503a0f16479ff3b7dd2a6ed
BryantShepherd <laituananh1711@gmail.com>
2018-10-19 21:39:32 +0700

    cap nhat

--- workspace/bai_tap_1.cpp
+++ workspace/bai_tap_1.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
-#include <math.h>
 using namespace std;
-
-string chuan_hoa(string a) {
-    if (a[0] >= 'a' && a[0] <= 'z') {
-        a[0] = a[0] - 32;
-    } 
-    for (int i = 1; i < a.length(); i++) {
-        if (a[i] >= 'A' && a[i] <= 'Z') {
-            a[i] = a[i] + 32;
-        }
-    } 
-    return a;
-}
 int main() {
-    string ten[3];
-    for (int i = 0; i < 3; i++) {
-        cin >> ten[i];
-    }
-    
-    for (int i = 0; i < 3; i++) {
-        cout << chuan_hoa(ten[i]) << " ";
-    }
-    return 0;
+	int so_phan_tu, max_index;
+	cin >> so_phan_tu;
+	int list[so_phan_tu];
+	for (int i = 0; i < so_phan_tu; i++) {
+		cin >> list[i];
+	}
+	for (int i = 0; i < so_phan_tu; i+=2) {
+		max_index = i;
+		for (int j = i + 2; j < so_phan_tu; j+=2) {
+			if (list[j] > list[max_index]) {
+				max_index = j;
+			}
+		}
+		int tmp = list[max_index];
+		list[max_index] = list[i];
+		list[i] = tmp;
+	}
+	for (int i = 0; i < so_phan_tu; i++) {
+		cout << list[i] << " ";
+	}
+	return 0;
 }
--- workspace/bai_tap_4.cpp
+++ workspace/bai_tap_4.cpp
@@ -1,32 +1,8 @@
 #include <iostream>
-
+#include <math.h>
 using namespace std;
+
 int main() {
-    double a, b, c;
-    cin >> a >> b >> c;
-    if ((a + b) > c && (b + c) > a && (a + c) > b) {
-
-        if (a == b && b == c) {
-            cout << "Tam giac deu.";
-        }
-        else if (a == b || b == c || a == c) {
-            if (a*a + b*b == c*c || b*b + c*c == a*a || a*a + c*c == b*b) {
-                cout << "Tam giac vuong can";
-            }
-            else {
-                cout << "Tam giac can";
-            }
-        }
-        else if (a*a + b*b == c*c || b*b + c*c == a*a || a*a + c*c == b*b) {
-            cout << "Tam giac vuong.";
-        }
-        else {
-            cout << "Tam giac thuong";
-        }
-
-    }
-    else {
-        cout << "Khong phai tam giac.";
-    }
+    cout << 1 % 2 << endl;
     return 0;
 }
