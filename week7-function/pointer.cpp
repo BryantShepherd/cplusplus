377529fde0782ddef85071b711f6aa6ee65e7fa1
BryantShepherd <laituananh1711@gmail.com>
2018-10-25 21:48:36 +0700

    chinh su nho

--- week7-function/pointer.cpp
+++ week7-function/pointer.cpp
@@ -2,11 +2,9 @@
 using namespace std;
 int main() {
     int number = 20;
-
-    int *pointer = &number; //khai bao con tro
+    int* pointer = &number; //khai bao con tro
 
     cout << number << endl; // in ra 20
-
     cout << pointer << endl; //in ra dia chi bien number
     cout << *pointer << endl; // in ra gia tri bien number(20)
 
