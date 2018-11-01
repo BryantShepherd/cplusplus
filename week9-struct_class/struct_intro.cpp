7a19bf6146993edfea585cd9773f17f1cc7bcfdd
BryantShepherd <laituananh1711@gmail.com>
2018-11-01 21:48:20 +0700

    cin struct

--- week9-struct_class/struct_intro.cpp
+++ week9-struct_class/struct_intro.cpp
@@ -6,13 +6,17 @@ struct SVector{
     double x;
     double y;
     
-    SVector(double _x, double _y) {
+    SVector(double _x, double _y) { //CHUA BIET DE LAM GI
         x = _x;
         y = _y;
     }
-    
+
     void print(){
         cout << "("<< x << ", " << y << ")\n";
+    }
+
+    void lay_du_lieu() {
+        cin >> x >> y;
     }
 };
 
@@ -29,9 +33,10 @@ class CVector{
 
 int main()
 {
-    //Khởi tạo dùng initialization list
+    //Khởi tạo dùng initialization list (BAT BUOC PHAI CO GIA TRI KHOI TAO VI MOT LY DO NAO DO...)
     SVector s = {1.0, 3.0};
     CVector c = {2.0, 5.0};
+    s.lay_du_lieu();
     
     // Hoặc là dùng uniform initialization (C++11)
     //uniform initialization, C++11~
@@ -39,10 +44,10 @@ int main()
     // CVector c {2.0, 5.0};
     
     //print vectors
-    s.print();
+    s.print(); //goi ham print trong struct SVector
     c.print();
     
-    cout << "Struct: x="<< s.x << ", y=" << s.y << "\n";
+    cout << "Struct: x="<< s.x << ", y=" << s.y << "\n"; //s.x la gia tri cua x trong
     
     cout << "Class: x= "<< c.x << ", y=" << c.y << "\n";
     
