2627dc61d34e5046d586b6cb17a2366ce7bf7204
BryantShepherd <laituananh1711@gmail.com>
2018-11-04 13:33:14 +0700

    bai tap codehub

--- workspace/bai_tap_3.cpp
+++ workspace/bai_tap_3.cpp
@@ -4,9 +4,17 @@
 using namespace std;
 
 int main() {
-    int a = 10;
-    for (int i = 0; i < a; i++) {
-        cout << i;
-        a--;
+    string a;
+    cin >> a;
+    string a_copy = a;
+    cout << a_copy.length()
+    for (int i = 0; i < a.length(); i++) {
+        if (a[i] <= 122 && a[i] >= 97) {
+            a[i]-=32;
+            a_copy.erase(a_copy.begin() + i);
+        }
+
     }
+    cout << a << " " << a_copy << endl;
+    // if ((a.find(b) != std::string::npos) || a_copy == b) cout << "ok" << endl;
 }
--- workspace/bai_tap_4.cpp
+++ workspace/bai_tap_4.cpp
@@ -1,114 +1,27 @@
 #include <iostream>
-<<<<<<< HEAD
-#include <algorithm> // for std::swap, use <utility> instead if C++11
- 
-
-bool ascending(int x, int y)
-=======
-<<<<<<< HEAD
-using namespace std;
-int main() {
-	int m, n;
-	cin >> m >> n;
-	int matrix[m][n];
-	
-	for (int i = 0; i < m; i++) {
-		for (int j = 0; j < n; j++) {
-			cin >> matrix[i][j];
-		}
-	}
-	
-    int hang_xoa;
-    cin >> hang_xoa;
-
-    for (int i = hang_xoa - 1; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            matrix[i][j] = matrix[i+1][j];
-        }
-    }
-
-    for (int i = 0; i < m - 1; i++) {
-        for (int j = 0; j < n; j++ ) {
-            cout << matrix[i][j] << " ";
-        }
-        cout << endl;
-    }
-	return 0;
-}
-=======
 #include <string>
-#include <sstream>
-#include <cmath>
 using namespace std;
-int main ()
->>>>>>> d34420de2d913b6a891b04e5ea4ae24b29acd63b
-{
-    return x > y;
-}
-
-bool descending(int x, int y)
-{
-    return x < y;
-}
-<<<<<<< HEAD
-
-//sort even numbers first by ascending orders
-bool evensFirst(int x, int y)
-{
-    // if x is even and y is odd, x goes first (no swap needed)
-    if ((x % 2 == 0) && !(y % 2 == 0))
-        return false;
- 
-    // if x is odd and y is even, y goes first (swap needed)
-    if (!(x % 2 == 0) && (y % 2 == 0))
-        return true;
- 
-        // otherwise sort in ascending order
-    return ascending(x, y);
-}
-
-void selectionSort(int *array, int size, bool (*comparisonFunc)(int, int))
-{
-    // Step through each element of the array
-    for (int startIndex = 0; startIndex < size; ++startIndex)
-    {
-        int smallestIndex = startIndex;
- 
-        // Look for smallest element remaining in the array (starting at startIndex+1)
-        for (int currentIndex = startIndex + 1; currentIndex < size; ++currentIndex)
-        {
-            if (comparisonFunc(array[smallestIndex], array[currentIndex])) // COMPARISON DONE HERE
-                // This is the new smallest number for this iteration
-                smallestIndex = currentIndex;
+bool kiem_tra(string a, string b) {
+    int k = 0;
+    for (int i = 0; i < a.length(); i++) {
+        if (a[i] >= 'a' && a[i] <= 'z') {
+            a[i]-=32;
         }
- 
-        // Swap our start element with our smallest element
-        std::swap(array[startIndex], array[smallestIndex]);
+        if (a[i] = b[k]) {
+            k++;
+            if (k == b.length()) return true;
+        }
+
+    }
+    return false;
+}
+int main() {
+    string a, b;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a >> b;
+        if (kiem_tra(a, b)) cout << "YES" << endl;
+        else cout << "NO" << endl;
     }
 }
-
-
-void printArray(int *array, int size)
-{
-    for (int index=0; index < size; ++index)
-        std::cout << array[index] << " ";
-    std::cout << '\n';
-}
-
-
-int main()
-{
-    int array[9] = { 3, 7, 9, 5, 6, 1, 8, 2, 4 };
- 
-    selectionSort(array, 9, descending);
-    printArray(array, 9);
-    
- 
-    selectionSort(array, 9, evensFirst);
-    printArray(array, 9);
-    
-    return 0;
-}
-=======
->>>>>>> 60ec1b860d60ad5c9f02918bba52d8b3af59f446
->>>>>>> d34420de2d913b6a891b04e5ea4ae24b29acd63b
