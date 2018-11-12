b0e5fadae633f05f0521e9d3f02d90b352644892
BryantShepherd <laituananh1711@gmail.com>
2018-11-12 22:02:57 +0700

    Class Array

--- week5-array_vector/vector.cpp
+++ week5-array_vector/vector.cpp
@@ -1,5 +1,9 @@
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <vector> //can de su dung vector
+#include <algorithm> //chua ham sort; min, max; swap; reverse
 using namespace std;
+
 int main() {
     vector <int> example;
     int a;
@@ -7,7 +11,7 @@ int main() {
         cin >> a;
         example.push_back(a); //them gia tri a vao cuoi array
     }
-    sort(example.begin(), example.end());
+    sort(example.begin(), example.end()); //sort(example, example + n)
     cout << example[0] << " " << example[2];
     return 0;
 }
