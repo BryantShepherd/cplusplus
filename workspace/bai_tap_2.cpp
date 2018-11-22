dcbb85d68b0738ecc23892b1dfc7b95457f8ec10
BryantShepherd <laituananh1711@gmail.com>
2018-11-22 18:59:29 +0700

    laptop

--- workspace/bai_tap_2.cpp
+++ workspace/bai_tap_2.cpp
@@ -1,25 +1,22 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+int getSum(vector<int> numbers)
+{
+    int sum = 0;
+    for (int i: numbers)
+        sum = sum + i;
+    return sum;
+}
+
 int main()
 {
-    int m,n,Q;
-    cin >> m>> n>> Q; 
-    int a[m][n];
-    for (int i=0;i<m;++i)
-    {   for (int j=0;j<n;++j)
-        {
-            cin >> a[i][j];// cho de sai
-        }
-    }    
-    int b[Q][2];     
-    for (int l=0;l<Q;++l){
-        for (int k=0;k<2;++k){
-            cin >> b[l][k];
-            
-        }
-        cout << a[b[l][0] - 1][b[l][1] - 1] << endl;
-    }
-    // cout <<a[b[Q][2]];
+    vector <int> a;
+    a.push_back(10);
+    a.push_back(20);
+    cout << a[0] << endl;
+    cout << getSum(a) << endl;
     return 0;
     
 }
--- workspace/bai_tap_4.cpp
+++ workspace/bai_tap_4.cpp
@@ -3,12 +3,10 @@
 #include <iostream>
 using namespace std;
 
-std::string to_format(const int number) {
-    std::stringstream ss;
-    ss << std::setw(2) << std::setfill('0') << number;
-    return ss.str();
-}
+
 
 int main() {
-    cout << (6/5) * 5 << endl;
+    for (i = 1; i < 11; i++) {
+        cout << i << endl;
+    }
 }
