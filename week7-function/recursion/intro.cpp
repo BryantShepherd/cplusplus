8b5af2fbc08ce46854ed01d0dfb7efebe2d0ebc5
BryantShepherd <laituananh1711@gmail.com>
2018-11-24 14:41:48 +0700

    may tinh ban

--- week7-function/recursion/intro.cpp
+++ week7-function/recursion/intro.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int tong_le(int n) {
-    while (2*n + 1 > 0) {
+    while (2*n + 1  0) {
         return (2*n + 1) + tong_le(2*n - 1);    
     }
         
--- workspace/bai_tap_4.cpp
+++ workspace/bai_tap_4.cpp
@@ -1,14 +1,6 @@
-#include <sstream>
-#include <iomanip>
 #include <iostream>
 using namespace std;
 
-std::string to_format(const int number) {
-    std::stringstream ss;
-    ss << std::setw(2) << std::setfill('0') << number;
-    return ss.str();
-}
-
 int main() {
-    cout << (6/5) * 5 << endl;
+
 }
