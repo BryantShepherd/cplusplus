6eda345e5498f0fbf46db61ffd5feb67408eef87
BryantShepherd <laituananh1711@gmail.com>
2019-02-09 14:58:49 +0700

    time

--- workspace/bai_tap_4.cpp
+++ workspace/bai_tap_4.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
-struct BankAccount
-{
-	__int64 accountNumber;
-	__int64 balance;
-};
+#include <cmath>
+using namespace std;
+int main() {
+	int gio, phut, giay;
+	char x;
+	cin >> gio >> x >> phut >> x >> giay;
+	int time_1 = gio*60*60 + phut*60 + giay;
+	cin >> gio >> x >> phut >> x >> giay;
+	int time_2 = gio*60*60 + phut*60 + giay;
+	cout << time_1 << " " << time_2 << " " << endl; 
+	int thoi_gian = time_2 - time_1;
+	if (thoi_gian <= 0) thoi_gian += 24 * 60 * 60;
 
-int main()
-{
-	BankAccount myAccount = { 123456789, 50 }; // $50
-	BankAccount *pAccount = &myAccount; 
-
-	std::cout << "My bank account number: " << myAccount.accountNumber << std::endl;
-	std::cout << "My bank account number: " << pAccount->accountNumber << std::endl;
-
-	std::cout << "My balance: " << myAccount.balance << std::endl;
-	std::cout << "My balance: " << pAccount->balance << std::endl;
-
-	return 0;
+	int thoi_gian_gio = thoi_gian / (60*60);
+	int thoi_gian_phut = (thoi_gian % (thoi_gian_gio * 60 * 60))/60;
+	int thoi_gian_giay = thoi_gian - thoi_gian_gio * 60 * 60 - thoi_gian_phut * 60; 
+	cout << thoi_gian_gio << ":" << thoi_gian_phut << ":" << thoi_gian_giay;
 }
