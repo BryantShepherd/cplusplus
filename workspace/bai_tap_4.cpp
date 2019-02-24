15bb1039e4b1404a81fa4fc764ef32e5fc1e2f11
BryantShepherd <laituananh1711@gmail.com>
2019-02-24 10:17:44 +0700

    simple_calculator hoan thanh

--- workspace/bai_tap_4.cpp
+++ workspace/bai_tap_4.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
-#include <string>
+#include <cerrno>
 #include <cmath>
+#include <stdlib.h>
 using namespace std;
 int main() {
     const double PI = 3.14159265358979323846; 
-    double a;
-    cin >> a;
-    cout << pow(a, 3);
+    double a, b;
+    cin >> a >> b;
+    if (pow(a,b) == INFINITY) cout << "error" << endl;
+    if (pow(a,b) == NAN) cout << "nanno" << endl;
+    cout << pow(a, b) << endl;
+    // if (errno) cout << "errno" << endl;
+    // else if (errno == EDOM) cout << "domain error" << endl;
+    // else if (errno == ERANGE) cout << "range error" << endl;
 
 }
--- workspace/bai_tap_5.cpp
+++ workspace/bai_tap_5.cpp
@@ -7,6 +7,16 @@
 #include <cmath>
 
 using namespace std;
+
+void add(double &num, double &result);
+void subtract(double &num, double &result);
+void multiply(double &num, double &result);
+void divide(double &num, double &result);
+void power(double &num, double &result);
+bool checkIfInteger(double num);
+// void checkInputValid(double num);
+void funcCalculate(double &num, char f_trig);
+
 int main() {
     const char ADD  = '+';
     const char MINUS = '-';
@@ -20,62 +30,73 @@ int main() {
     const char QUIT = 'q';
     const char CLEAR = 'c';
     const char COSINE = 'c';
+    const char CLEAR_MEMORY = 'c';
     const char SINE = 's';
     const char TANGENT = 't';
+    const char REVOKE_MEMORY = 'r';
     const double PI = 3.14159265358979323846; //co san trong c++, ve nha tim 
     //khi nguoi dung nhap p thi gia tri p se thay bang pi?
     //define != const. define la no se tim tat ca su xuat hien cua pi va thay bang 3.14, const thi giong bien.
 
-    char op = '+', tmp_op, cal_mode = 'a', f_trig;
-    float num;
-    float result = 0;
-    float memory = 0;
+    char op = '+', tmp_op, cal_mode = 'a', f_trig, prev_op;
+    double num;
+    double result = 0;
+    double memory = 0;
     bool notQuit = true;
     bool luot_dau = false;
 
     cout << "   =" << result << endl;
     while(notQuit) {
         if (cal_mode == 'a') {
-            // if (luot_dau == false) {
-            //     luot_dau == true;
-            // }
-            // else cin >> op;
             cout << "Mode: A" << endl;
+            if (op != '=') prev_op = op;
             cin >> op;
             switch(op) {
                 //Math Operation
                 case ADD:
-                    cin >> num;
-                    //xu ly loi: neu nhap nham num se =0 lien tuc
-                    result += num;
-                    cout << "   =" << result << endl;
+                    // cin >> num;
+                    // //xu ly loi: neu nhap nham num se =0 lien tuc
+                    // result += num;
+                    // cout << "   =" << result << endl;
+                    add(num, result);
                     break;
                 
                 case MINUS:
-                    cin >> num;
-                    result -= num;
-                    cout << "   =" << result << endl;
+                    subtract(num, result);
                     break;
                 
                 case MULTIPLY:
-                    cin >> num;
-                    result *= num;
-                    cout << "   =" << result << endl;
+                    multiply(num, result);
                     break;
 
                 case DIVIDE:
-                    cin >> num;
-                    if (num == 0) {
-                        cout << "error: divided by zero" << endl;
-                        return 0;
-                    }
-                    else result /= num;
-                    cout << "   =" << result << endl;
+                    divide(num, result);
                     break;
 
                 case POWER:
-                    cin >> num;
-                    result = pow(result, num);
+                    power(num, result);
+                    break;
+
+                case EQUAL:
+                    switch(prev_op){
+                        case ADD:
+                            result += num;
+                            break;
+                        case MINUS:
+                            result -= num;
+                            break;
+                        case MULTIPLY:
+                            result *= num;
+                            break;
+                        case DIVIDE:
+                            result /= num;
+                            break;
+                        case POWER:
+                            result = pow(result, num);
+                            break;
+                        default:
+                            break;
+                    }
                     cout << "   =" << result << endl;
                     break;
                 
@@ -110,30 +131,20 @@ int main() {
 
         else if (cal_mode == 'f') {
             cout << "Mode: F" << endl;
+            if (op != '=') prev_op = op; //for EQUAL operator
             cin >> op;
             switch(op) {
-                //Math Operation
+                //Math Operation: 1. cho tat ca case vao funcAdd, funcSubtract,... 2. Them if vao ham add, subtract 3. Them phan switch(f_trig) vao funcCheck
                 case ADD:
                     cin >> f_trig;
                     cin >> num;
-                    num = num*PI/180;
-                    switch(f_trig){
-                        case SINE:
-                            num = sin(num);
-                            break;
-                        case COSINE:
-                            num = cos(num);
-                            break;
-                        
-                        case TANGENT:
-                            if (cos(num) == 0) {
-                                cout << "error: divided by zero" << endl;
-                                return 0;
-                            } 
-                            num = sin(num)/cos(num);
-       
+                    if (!cin) {
+                        cout << "error: invalid number" << endl;
+                        exit(EXIT_FAILURE);
                     }
-                    //xu ly loi: neu nhap nham num se =0 lien tuc
+
+                    num = num*PI/180;
+                    funcCalculate(num, f_trig);
                     result += num;
                     // cout.precision(2); t180 khong in ra 0 ma in ra so gan 0
                     cout << "   =" << result << endl;
@@ -142,24 +153,13 @@ int main() {
                 case MINUS:
                     cin >> f_trig;
                     cin >> num;
-                    num = num * PI / 180; 
-                    switch(f_trig){
-                        case SINE:
-                            num = sin(num);
-                            break;
-
-                        case COSINE:
-                            num = cos(num);
-                            break;
-                        
-                        case TANGENT:
-                            if (cos(num) == 0) {
-                                cout << "error: divided by zero" << endl;
-                                return 0;
-                            } 
-                            num = sin(num)/cos(num);
-       
+                    if (!cin) {
+                        cout << "error: invalid number" << endl;
+                        exit(EXIT_FAILURE);
                     }
+
+                    num = num * PI / 180; 
+                    funcCalculate(num, f_trig);
 
                     result -= num;
                     cout << "   =" << result << endl;
@@ -168,24 +168,13 @@ int main() {
                 case MULTIPLY:
                     cin >> f_trig;
                     cin >> num;
-                    num = num*PI/180;
-                    switch(f_trig){
-                        case SINE:
-                            num = sin(num);
-                            break;
-
-                        case COSINE:
-                            num = cos(num);
-                            break;
-                        
-                        case TANGENT:
-                            if (cos(num) == 0) {
-                                cout << "error: divided by zero" << endl;
-                                return 0;
-                            } 
-                            num = sin(num)/cos(num);
-       
+                    if (!cin) {
+                        cout << "error: invalid number" << endl;
+                        exit(EXIT_FAILURE);
                     }
+
+                    num = num*PI/180;
+                    funcCalculate(num, f_trig);
 
                     result *= num;
                     cout << "   =" << result << endl;
@@ -194,24 +183,13 @@ int main() {
                 case DIVIDE:
                     cin >> f_trig;
                     cin >> num;
-                    num = num*PI/180;
-                    switch(f_trig){
-                        case SINE:
-                            num = sin(num);
-                            break;
-
-                        case COSINE:
-                            num = cos(num);
-                            break;
-                        
-                        case TANGENT:
-                            if (cos(num) == 0) {
-                                cout << "error: divided by zero" << endl;
-                                return 0;
-                            } 
-                            num = sin(num)/cos(num);
+                    if (!cin) {
+                        cout << "error: invalid number" << endl;
+                        exit(EXIT_FAILURE);
                     }
 
+                    num = num*PI/180;
+                    funcCalculate(num, f_trig);
                     if (num == 0) {
                         cout << "error: divided by zero" << endl;
                         return 0;
@@ -223,26 +201,42 @@ int main() {
                 case POWER:
                     cin >> f_trig;
                     cin >> num;
-                    num = num*PI/180;
-                    switch(f_trig){
-                        case SINE:
-                            num = sin(num);
-                            break;
-
-                        case COSINE:
-                            num = cos(num);
-                            break;
-                        
-                        case TANGENT:
-                            if (cos(num) == 0) {
-                                cout << "error: divided by zero" << endl;
-                                return 0;
-                            } 
-                            num = sin(num)/cos(num);
-       
+                    if (!cin) {
+                        cout << "error: invalid number" << endl;
+                        exit(EXIT_FAILURE);
                     }
-                    //xu ly loi: result khong ra ket qua
+
+                    num = num*PI/180;
+                    funcCalculate(num, f_trig);
+
+                    if ((result == 0 && num <= 0) || (result < 0 && !checkIfInteger(num))){
+                        cout << "error: MATH error" << endl;
+                        exit(EXIT_FAILURE);
+                    }
                     result = pow(result, num);
+                    cout << "   =" << result << endl;
+                    break;
+
+                case EQUAL:
+                    switch(prev_op){
+                        case ADD:
+                            result += num;
+                            break;
+                        case MINUS:
+                            result -= num;
+                            break;
+                        case MULTIPLY:
+                            result *= num;
+                            break;
+                        case DIVIDE:
+                            result /= num;
+                            break;
+                        case POWER:
+                            result = pow(result, num);
+                            break;
+                        default:
+                            break;
+                    }
                     cout << "   =" << result << endl;
                     break;
                 
@@ -279,11 +273,138 @@ int main() {
         else if (cal_mode == 'm') {
             cout << "Mode: M" << endl;
             cin >> op;
-            // switch(op) {
+            switch(op) {
+                case ADD:
+                    memory += result;
+                    cout << "   =" << memory << endl;
+                    // cal_mode = 'a';
+                    break;
+                
+                case MINUS:
+                    memory -= result;
+                    cout << "   =" << memory << endl;
+                    break;
 
-            // }
+                case REVOKE_MEMORY:
+                    cout << "   =" << memory << endl;
+                    break;
+
+                case CLEAR_MEMORY:
+                    memory = 0;
+                    cout << "   =" << memory << endl;
+                    break;
+
+                //System
+                case QUIT:
+                    return 0;
+
+                //Change Mode
+                case A_MODE:
+                    cal_mode = 'a';
+                    break;
+
+                case F_MODE:
+                    cal_mode = 'f';
+                    break;
+
+                case M_MODE:
+                    cal_mode = 'm';
+                    break;
+                    
+                default:
+                    cout << "error: unidentified operator" << endl;
+                    break;
+            }
         }
     }
 }
 
+void add(double &num, double &result){
+    cin >> num;
+    if (!cin) {
+        cout << "error: invalid number" << endl;
+        exit(EXIT_FAILURE);
+    }
+    result += num;
+    cout << "   =" << result << endl;
+}
 
+void subtract(double &num, double &result){
+    cin >> num;
+    if (!cin) {
+        cout << "error: invalid number" << endl;
+        exit(EXIT_FAILURE);
+    }
+    result -= num;
+    cout << "   =" << result << endl;
+}
+
+void multiply(double &num, double &result){
+    cin >> num;
+    if (!cin) {
+        cout << "error: invalid number" << endl;
+        exit(EXIT_FAILURE);
+    }
+    result *= num;
+    cout << "   =" << result << endl;
+}
+
+void divide(double &num, double &result){
+    cin >> num;
+    if (!cin) {
+        cout << "error: invalid number" << endl;
+        exit(EXIT_FAILURE);
+    }
+    if (num == 0) {
+        cout << "error: divided by zero" << endl;
+        exit(EXIT_FAILURE);
+    }
+    result /= num;
+    cout << "   =" << result << endl;
+}
+
+void power(double &num, double &result){
+    cin >> num;
+    if (!cin) {
+        cout << "error: invalid number" << endl;
+        exit(EXIT_FAILURE);
+    }
+    if ((result == 0 && num <= 0) || (result < 0 && !checkIfInteger(num))){
+        cout << "error: MATH error" << endl;
+        exit(EXIT_FAILURE);
+    }
+    result = pow(result, num);
+    cout << "   =" << result << endl;
+}
+
+bool checkIfInteger(double num) {
+    if (floor(num) == ceil(num)) return true;
+    else return false;
+}
+
+void funcCalculate(double &num, char f_trig) {
+    const char COSINE = 'c';
+    const char SINE = 's';
+    const char TANGENT = 't';
+    switch(f_trig){
+        case SINE:
+            num = sin(num);
+            break;
+
+        case COSINE:
+            num = cos(num);
+            break;
+        
+        case TANGENT:
+            if (cos(num) == 0) {
+                cout << "error: divided by zero" << endl;
+                exit(EXIT_FAILURE);
+            } 
+            num = sin(num)/cos(num);
+            break;
+
+        default: 
+            cout << "error: unidentified operator" << endl;
+            exit(EXIT_FAILURE);
+    }
+}
